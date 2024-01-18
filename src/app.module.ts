e09c897a4d0c9c70a7bd2ee1fe670b6c59d6a691
Bob Farias <bobhenrique9@gmail.com>
2024-01-18 20:50:07 +0000

    [ADD] added swagger to the project [ADD] connection with the database postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AutomationModule } from './app/automation/automation.module';
+
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const CONFIG_DATABASE: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '261196',
+  database: 'automation-api-db',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(CONFIG_DATABASE), AutomationModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// SwaggerModule and DocumentBuilder are used for setting up Swagger (OpenAPI) documentation.
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuration for the Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Automation API')
+    .setDescription('The automation API description')
+    .setVersion('1.0')
+    .addTag('automation')
+    .build();
+
+  // Generating documentation with  the application and the configuration
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
